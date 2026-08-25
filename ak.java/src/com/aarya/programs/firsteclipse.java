567c7f548d245169e8a20c87c97410e3b842901f
codebyaarya <aaryakale920@gmail.com>
2026-08-26 00:45:56 +0530

    Add files via upload

---
+++ ak.java/src/com/aarya/programs/firsteclipse.java
@@ -0,0 +1,8 @@
+package com.aarya.programs;
+
+public class firsteclipse {
+	public static void main(String[] args) {
+		System.out.println("First program in java eclipse");
+	}
+
+}
---
+++ ak.java/src/com/aarya/programs/sum.java
@@ -0,0 +1,22 @@
+package com.aarya.programs;
+import java.util.Scanner;
+
+public class sum {
+	public static void main(String []args) {
+		int x,y,z;
+		Scanner sc =new Scanner(System.in);
+		System.out.println("enter no 1");
+		x=sc.nextInt();
+		System.out.println("enter no 2");
+	
+		y=sc.nextInt();
+		z=x+y;
+		System.out.println("Sum is " +z);
+		
+		
+		
+	}
+	
+	
+
+}
